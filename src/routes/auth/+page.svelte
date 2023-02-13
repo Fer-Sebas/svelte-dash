884c1f01b0f1d20597d9aaf24ad774b9ebaa6ae7
Sebastian Ferreira <io@fersebas.com>
2023-02-12 21:13:52 -0500

    Update UI

---
+++ src/routes/auth/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+ 
+    import Dialog from "../../components/containers/Dialog.svelte";
+	import InputPassword from "../../components/InputPassword.svelte";
+	import InputText from "../../components/InputText.svelte";
+
+    export let credentials = { username: '', password: '' }
+
+</script>
+
+<Dialog>
+    <form>
+        <InputText name='username' valueName={credentials.username}/>
+        <InputPassword name='password' valueName={credentials.password}/>
+        <button> Log In </button>
+    </form>
+</Dialog>
