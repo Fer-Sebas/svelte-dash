569ddb7a5aac2d3f492d180cb6e0c9b9075e081c
Sebastian Ferreira <io@fersebas.com>
2023-02-12 21:12:53 -0500

    Include new component

---
+++ src/components/InputText.svelte
@@ -0,0 +1,11 @@
+<script lang='ts'>
+
+    export let name : string
+    export let inputValue : string = ''
+    
+</script>
+
+<div>
+    <label for="{name}">{name}</label>
+    <input type="text" name="{name}" bind:value={inputValue}>
+</div>
