19b9e54e601b0070fd31ecdbeb38da4c445eb355
Sebastian Ferreira <io@fersebas.com>
2023-02-25 13:53:54 -0500

    Update/Include component

---
+++ src/components/controllers/InputNumber.svelte
@@ -0,0 +1,11 @@
+<script lang='ts'>
+
+    export let name : string
+    export let inputValue : number = 0
+    
+</script>
+
+<div>
+    <label for="{name}">{name}</label>
+    <input type="number" name="{name}" bind:value={inputValue}>
+</div>
---
+++ src/components/controllers/InputPassword.svelte
@@ -0,0 +1,11 @@
+<script lang='ts'>
+
+    export let name : string
+    export let inputValue : string = ''
+    
+</script>
+
+<div>
+    <label for="{name}">{name}</label>
+    <input type="password" name="{name}" bind:value={inputValue}>
+</div>
